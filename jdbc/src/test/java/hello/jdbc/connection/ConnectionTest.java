3040e129c299446ab74169c99a9207d8bbb60727
power30111 <power30111@naver.com>
2023-04-28 17:53:48 +0900

    ConnectionPool, DataSource

---
+++ jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -0,0 +1,52 @@
+package hello.jdbc.connection;
+
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+
+@Slf4j
+public class ConnectionTest {
+
+    @Test
+    void driverManager() throws SQLException {
+        Connection con1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Connection con2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        log.info("connection={}, class={}",con1,con1.getClass());
+        log.info("connection={}, class={}",con2,con2.getClass());
+    }
+
+    @Test
+    void dataSourceDriverManager() throws SQLException {
+        //DriverManagerDataSource - 항상 새로운 커넥션을 획득
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        useDataSource(dataSource);
+    }
+    @Test
+    void dataSourceConnectionPool() throws SQLException {
+        //커넥션 풀링
+        HikariDataSource DataSource = new HikariDataSource();
+        DataSource.setJdbcUrl(URL);
+        DataSource.setUsername(USERNAME);
+        DataSource.setPassword(PASSWORD);
+        DataSource.setMaximumPoolSize(10);
+        DataSource.setPoolName("MyPool");
+
+        useDataSource(DataSource);
+    }
+
+
+    private void useDataSource(DataSource dataSource) throws SQLException {
+        Connection con1 = dataSource.getConnection();
+        Connection con2 = dataSource.getConnection();
+        log.info("connection={}, class={}",con1,con1.getClass());
+        log.info("connection={}, class={}",con2,con2.getClass());
+    }
+}
