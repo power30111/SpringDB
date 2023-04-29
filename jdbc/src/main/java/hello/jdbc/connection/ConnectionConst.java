8d39c0892db9ba1818887bac79e4055bd9d05046
power30111_HOME <power30111@naver.com>
2023-04-29 20:59:17 +0900

    update

--- jdbc/src/main/java/hello/jdbc/connection/ConnectionConst.java
+++ jdbc/src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -1,6 +1,7 @@
 package hello.jdbc.connection;
 
 public abstract class ConnectionConst {
+    //미리 자주사용하는 것들을 상수화시킴
     public static final String URL = "jdbc:h2:tcp://localhost/~/test";
     public static final String USERNAME="sa";
     public static final String PASSWORD ="";
