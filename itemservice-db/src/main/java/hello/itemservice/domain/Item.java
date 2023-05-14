e7a89ac0a25dd8b263cdd38e22a240d036dda2e3
power30111 <power30111@naver.com>
2023-05-14 21:35:44 +0900

    JPA

--- itemservice-db/src/main/java/hello/itemservice/domain/Item.java
+++ itemservice-db/src/main/java/hello/itemservice/domain/Item.java
@@ -2,11 +2,17 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity //JPA에서 관리하는 객체 (JPA가 인식가능)
+//@Table(name = "Item") 객체명과 테이블명이 같을경우 생략가능
 public class Item {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;    //P.K 라는것을 명시
 
-    private Long id;
-
+    @Column(name = "item_name", length = 10)    //Column 명시
+    //Column(name 설정할때 camleCase <-> snake_case 간 변환을 알잘딱해줘서 사실생략가능
     private String itemName;
     private Integer price;
     private Integer quantity;
