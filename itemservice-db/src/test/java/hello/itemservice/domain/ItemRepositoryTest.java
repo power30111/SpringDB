b706266273f962778af9d8878a8caf493fe10750
power30111 <power30111@naver.com>
2023-05-14 23:01:42 +0900

    JPA TEST update

--- itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -4,6 +4,7 @@ import hello.itemservice.repository.ItemRepository;
 import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import hello.itemservice.repository.memory.MemoryItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+@Slf4j
 @Transactional          //어노테이션으로 트랜잭션 적용 -> test코드가 아닐경우엔 문제없을시 commit하는데 test코드에서는 죄다 롤백하는듯0
 @SpringBootTest         //상위폴더로 올라가면서 SpringBootApplication 을 찾은뒤 설정을 그대로 가져다 사용
 class ItemRepositoryTest {
@@ -77,6 +78,7 @@ class ItemRepositoryTest {
         Item item2 = new Item("itemA-2", 20000, 20);
         Item item3 = new Item("itemB-1", 30000, 30);
 
+        log.info("repository={}",itemRepository.getClass());
         itemRepository.save(item1);
         itemRepository.save(item2);
         itemRepository.save(item3);
